0a159f9b6f8c4c7f8db8c476bc09f01ff5b0b10e
Daniel Petrov <daniil_petrov19@icloud.com>
2025-07-16 22:57:56 +0700

    change model to 2.1

--- app/agent.py
+++ app/agent.py
@@ -2,7 +2,7 @@ from langchain_ollama import OllamaLLM
 from langchain.agents import initialize_agent, AgentType
 from tools import tools
 
-llm = OllamaLLM(model="llama3:8b", base_url="http://ollama:11434")  # или другое имя, если ты загрузил с другим названием
+llm = OllamaLLM(model="llama3.1:8b", base_url="http://ollama:11434")  # или другое имя, если ты загрузил с другим названием
 
 agent = initialize_agent(
     tools=tools,
